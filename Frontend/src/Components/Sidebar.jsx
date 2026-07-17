0eab031b966f302e043d82915978cafe40c4941c
shivam prakash <shivamprakash303@gmail.com>
2026-07-17 22:12:37 +0530

    responsive corrections

--- Frontend/src/Components/Sidebar.jsx
+++ Frontend/src/Components/Sidebar.jsx
@@ -10,12 +10,13 @@ const Sidebar = () => {
     const { logout, onlineUsers } = useContext(AuthContext);
     const { selectedUser, setSelectedUser, getUsers, users, unseenMessages, setUnseenMessages } = useContext(ChatContext);
     const [input, setInput] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
     const nevigate = useNavigate();
 
-    const filteredUser = input ? users.filter((user)=>user.fullName.toLowerCase().includes(input.toLowerCase())) : users;
-    useEffect(()=>{
+    const filteredUser = input ? users.filter((user) => user.fullName.toLowerCase().includes(input.toLowerCase())) : users;
+    useEffect(() => {
         getUsers()
-    },[onlineUsers])
+    }, [onlineUsers])
 
     return (
         <div className={`${selectedUser ? "hidden" : "flex"} md:flex bg-(--border) rounded-lg py-2 px-4 h-full flex-col gap-7 overflow-scroll`}>
@@ -26,30 +27,30 @@ const Sidebar = () => {
                         <h1 className="text-xl">Zap Chat</h1>
                     </div>
                     <div className="relative py-2 group">
-                        <img src={assets.menu} className="h-8 w-8 cursor-pointer" alt="" />
-                        <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-(--bg) border border-gray-600 hidden group-hover:block">
-                            <p className="cursor-pointer text-sm" onClick={()=>nevigate("/profile")}>Edit Profile</p>
-                            <hr className="my-2 border-t border-gray-500"/>
-                            <p className="cursor-pointer text-sm" onClick={()=> logout()}>Logout</p>
-                        </div>
+                        <img src={assets.menu} className="h-8 w-8 cursor-pointer" alt="" onClick={()=>showMenu ? setShowMenu(false) : setShowMenu(true)}/>
+                        {showMenu && <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-(--bg) border border-gray-600">
+                            <p className="cursor-pointer text-sm" onClick={() => nevigate("/profile")}>Edit Profile</p>
+                            <hr className="my-2 border-t border-gray-500" />
+                            <p className="cursor-pointer text-sm" onClick={() => logout()}>Logout</p>
+                        </div>}
                     </div>
                 </div>
                 <div className="bg-(--bg) pl-3 pt-2 pb-2 rounded-3xl flex gap-3 items-center">
                     <i className="fa-solid fa-magnifying-glass"></i>
-                    <input onChange={(e)=>setInput(e.target.value)} type="text" className="border-none outline-none text-white placeholder-gray-300" placeholder="search here..." />
+                    <input onChange={(e) => setInput(e.target.value)} type="text" className="border-none outline-none text-white placeholder-gray-300" placeholder="search here..." />
                 </div>
             </div>
             <div className="flex flex-col gap-2 overflow-y-scroll h-full">
                 {filteredUser.map((user, index) => {
                     return (
-                        <div key={user._id} className={`relative flex items-center rounded-xl px-4 py-2 gap-2 cursor-pointer ${selectedUser?.id === user.id && "bg-(--code-bg)"}`} onClick={() => { setSelectedUser(user); setUnseenMessages((prev)=>({...prev, [user._id] : 0}))}}>
+                        <div key={user._id} className={`relative flex items-center rounded-xl px-4 py-2 gap-2 cursor-pointer ${selectedUser?.id === user.id && "bg-(--code-bg)"}`} onClick={() => { setSelectedUser(user); setUnseenMessages((prev) => ({ ...prev, [user._id]: 0 })) }}>
                             <img src={user.profilePic || assets.profile} className="h-15 w-15 rounded-full" alt="" />
                             <div>
                                 <h1>{user.fullName}</h1>
                                 {
                                     onlineUsers.includes(user._id) ?
-                                    <p className="text-sm text-green-500">Online</p> :
-                                    <p className="text-sm">Offline</p>
+                                        <p className="text-sm text-green-500">Online</p> :
+                                        <p className="text-sm">Offline</p>
                                 }
                             </div>
                             {
--- Frontend/src/pages/Loginpage.jsx
+++ Frontend/src/pages/Loginpage.jsx
@@ -12,28 +12,28 @@ const Loginpage = () => {
   const navigate = useNavigate();
 
   const { login } = useContext(AuthContext);
-  
-  const submitHandler = (e) =>{
+
+  const submitHandler = (e) => {
     e.preventDefault();
 
-    if(!isLogin && !isSumitted){
+    if (!isLogin && !isSumitted) {
       setIsSubmitted(true);
       return;
     }
-    login(isLogin ? "login" : "signin", {fullName, email, password});
+    login(isLogin ? "login" : "signin", { fullName, email, password });
     navigate("/");
   }
   return (
-    <form className="grid grid-cols-2 md:grid-cols-[2fr_1fr] h-full p-15 items-center" onSubmit={submitHandler}>
+    <form className="grid gap-4 md:grid-cols-[2fr_1fr] h-full p-5 md:p-15 items-center" onSubmit={submitHandler}>
       <div className="flex flex-col items-center justify-center h-full">
-        <img src={assets.logo} className="h-40 w-50" alt="" />
-        <h1 className="text-4xl">Zap Chat</h1>
+        <img src={assets.logo} className="hidden md:block md:h-40 md:w-50" alt="" />
+        <h1 className="text-4xl text-(--accent)">Zap Chat</h1>
       </div>
       <div className="bg-(--border) rounded-lg p-5 flex flex-col gap-2">
         {isLogin ? <h1 className="mb-4 text-xl">Login</h1> : <h1 className="mb-4 text-xl">Sign Up</h1>}
-        {!isLogin ? <input type="text" className="bg-(--text) py-2 px-4 my-1 rounded-lg text-black text-sm outline-none" placeholder="Full Name" value={fullName} onChange={(e)=> setFullName(e.target.value)} required/> : ""}
-        <input type="email" className="bg-(--text) py-2 px-4 my-1 rounded-lg text-black text-sm outline-none" placeholder="Email Address" value={email} onChange={(e)=> setEmail(e.target.value)} required/>
-        <input type="password" className="bg-(--text) py-2 px-4 my-1 rounded-lg text-black text-sm outline-none" placeholder="Password" value={password} onChange={(e)=> setPassword(e.target.value)} required/>
+        {!isLogin ? <input type="text" className="bg-(--text) py-2 px-4 my-1 rounded-lg text-black text-sm outline-none" placeholder="Full Name" value={fullName} onChange={(e) => setFullName(e.target.value)} required /> : ""}
+        <input type="email" className="bg-(--text) py-2 px-4 my-1 rounded-lg text-black text-sm outline-none" placeholder="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="password" className="bg-(--text) py-2 px-4 my-1 rounded-lg text-black text-sm outline-none" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         {
           isLogin ? <button type="submit" className="bg-(--accent) py-2 px-4 rounded-lg my-1 text-black outline-none">Login</button> : <button type="submit" className="bg-(--accent) py-2 px-4 rounded-lg my-1 text-black outline-none">Create Account</button>
         }
@@ -41,9 +41,9 @@ const Loginpage = () => {
           <input type="checkbox" />
           <p className="text-xs">Agree to the terms of use & privacy policy</p>
         </div>
-        {isLogin ? 
-          <p className="text-sm mt-4">Create an Account? <span onClick={()=>{setIsLogin(false)}} className="text-blue-500 cursor-pointer">Click Here</span></p> :
-          <p className="text-sm mt-4">Already have an account? <span onClick={()=>{setIsLogin(true)}} className="text-blue-500 cursor-pointer">Login here</span></p> 
+        {isLogin ?
+          <p className="text-sm mt-4">Create an Account? <span onClick={() => { setIsLogin(false) }} className="text-blue-500 cursor-pointer">Click Here</span></p> :
+          <p className="text-sm mt-4">Already have an account? <span onClick={() => { setIsLogin(true) }} className="text-blue-500 cursor-pointer">Login here</span></p>
         }
       </div>
     </form>
