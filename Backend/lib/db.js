c05359f1601afe4563a7432ccdb761b9ef2040ef
shivam prakash <shivamprakash303@gmail.com>
2026-07-17 21:39:11 +0530

    db error check

--- Backend/lib/db.js
+++ Backend/lib/db.js
@@ -6,6 +6,6 @@ export const connectDB = async()=>{
             console.log("DB connected");
         });
     } catch (error) {
-        console.log("Error in DB connection: ", error);
+        console.log("Error in DB connection: ", error.message);
     }
 }
